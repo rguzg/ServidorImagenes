e8bbddb088f7128507eecd0c250871d64ff54255
Raúl <raulguzmanguerrero@outlook.com>
2021-02-11 17:56:45 -0600

    Arreglar uso intensivo de CPU después de enviar una imagen

--- Servidor.py
+++ Servidor.py
@@ -1,25 +1,29 @@
 from socket import socket, error
 
 def main():
-    s = socket()
-
-    s.bind(("localhost", 6030))
-    s.listen(0)
-
-    print("El servidor esta listo")
-
-    conn, addr = s.accept()
-    f = open("recibido.png", "wb")
-
     while True:
-        try:
-            # Recibir datos del cliente.
-            input_data = conn.recv(1024)
-        except error:
-            print("Error de lectura.")
-            break
-        else:
+        s = socket()
+
+        s.bind(("localhost", 6030))
+        s.listen(0)
+
+        print("El servidor esta listo")
+
+        conn, addr = s.accept()
+        f = open("recibido.jpg", "wb")
+
+
+# Cambiar esto por algo que tenga más forma: https://docs.python.org/3/library/socket.html#example
+        while True:
+            try:
+                # Recibir datos del cliente.
+                input_data = conn.recv(1024)
+            except error:
+                print("Error de lectura.")
+                break
+
             if input_data:
+                # Mover esto a otro lado para que el archivo no se corrompa (Implementar lo de nombres aleatorios?)
                 # Compatibilidad con Python 3.
                 if isinstance(input_data, bytes):
                     end = input_data[0] == 1
@@ -29,8 +33,12 @@ def main():
                     # Almacenar datos.
                     f.write(input_data)
                 else:
+                    print("Imagen guardada")
+                    f.close()
                     break
-    f.close()
+            else:
+                print("Imagen guardada")
+                break
 
 
 if __name__ == "__main__":
