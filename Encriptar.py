d58b41c66eceb87141658f9ab59fc36b5a6ce400
Emiliano Zuchovicki <emilianozuchovicki@hotmail.com>
2021-02-22 11:08:28 -0600

    Cambio A Funciones de Encriptar/Desencriptar

--- Encriptar.py
+++ Encriptar.py
@@ -1,63 +1,63 @@
 # Encriptación
 # Path: C:\Users\aniav\Downloads\Gato.jpg
 # Key: 70
+def Encriptar(img):
+    try:
+        # Path de la imagen
+        path = img
 
-try:
-    # Path de la imagen
-    path = input('Ingresa la ruta de la imagen: ')
+        # Llave de encriptación
+        key = 70
 
-    # Llave de encriptación
-    key = 70
+        # Abrir el archivo
+        archivo = open(path, 'rb')
 
-    # Abrir el archivo
-    archivo = open(path, 'rb')
+        imagen = archivo.read()
+        archivo.close()
 
-    imagen = archivo.read()
-    archivo.close()
+        # Encriptación
+        imagen = bytearray(imagen)
 
-    # Encriptación
-    imagen = bytearray(imagen)
+        # Operación XOR
+        for indice, valores in enumerate(imagen):
+            imagen[indice] = valores ^ key
 
-    # Operación XOR
-    for indice, valores in enumerate(imagen):
-        imagen[indice] = valores ^ key
+        # Abrir archivo para escribir encriptación
+        archivo = open(path, 'wb')
+        archivo.write(imagen)
+        archivo.close()
+        print("Imagen Encriptada")
 
-    # Abrir archivo para escribir encriptación
-    archivo = open(path, 'wb')
-    archivo.write(imagen)
-    archivo.close()
-    print("Imagen Encriptada")
-
-except Exception:
-    print("Error: ", Exception.__name__)
+    except Exception:
+        print("Error: ", Exception.__name__)
 
 
 # Desencriptación
 # Para desencriptar la imagen se necesita ingresar la misma llave como clave
+def Desencriptar(img):
+    try:
+        # Ingresar ruta de la imagen a desencriptar
+        path = img
 
-# try:
-#     # Ingresar ruta de la imagen a desencriptar
-#     path = input('Ingresa la ruta de la imagen: ')
+        key = 70
 
-#     key = 70
+        # Abrir el archivo para leer
+        archivo = open(path, 'rb')
+        imagen = archivo.read()
+        archivo.close()
 
-#     # Abrir el archivo para leer
-#     archivo = open(path, 'rb')
-#     imagen = archivo.read()
-#     archivo.close()
+        # Desencriptación
+        imagen = bytearray(imagen)
 
-#     # Desencriptación
-#     imagen = bytearray(imagen)
+        # Operación XOR
+        for indice, valores in enumerate(imagen):
+            imagen[indice] = valores ^ key
 
-#     # Operación XOR
-#     for indice, valores in enumerate(imagen):
-#         imagen[indice] = valores ^ key
+        # Abrir archivo para escribir desencriptación
+        archivo = open(path, 'wb')
+        archivo.write(imagen)
+        archivo.close()
+        print("Imagen Desencriptada")
 
-#     # Abrir archivo para escribir desencriptación
-#     archivo = open(path, 'wb')
-#     archivo.write(imagen)
-#     archivo.close()
-#     print("Imagen Desencriptada")
-
-# except Exception:
-#     print("Error: ", Exception.__name__)
+    except Exception:
+        print("Error: ", Exception.__name__)
